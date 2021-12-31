00c05336bc9cb849d9329c0dfb676e1106d22fa2
Kiruba Shankar Jayakumar <jkiruba.sankar@gmail.com>
2021-12-31 12:07:39 +0530

    angular heaader and footer

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
@@ -9,14 +10,14 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
-
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish){
