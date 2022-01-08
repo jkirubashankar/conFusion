6643498ea8fa6b912185134e0192170810c1e00b
Kiruba Shankar Jayakumar <jkiruba.sankar@gmail.com>
2022-01-08 12:52:02 +0530

    angular reactive forms rxjs

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,27 +11,30 @@ export class LeaderService {
   
   constructor() { }
 
-  getLeaders(): Promise<Leader[]>{
-    return new Promise(resolve=> {
-      setTimeout(() => {
-        resolve(LEADERS)
-      }, 2000);
-    });  
+  getLeaders(): Observable<Leader[]>{
+    return of(LEADERS).pipe(delay(2000));
+    // return new Promise(resolve=> {
+    //   setTimeout(() => {
+    //     resolve(LEADERS)
+    //   }, 2000);
+    // });  
   }
 
-  getLeader(id: string): Promise<Leader>{
-    return new Promise(resolve=> {
-      setTimeout(() => {
-        resolve(LEADERS.filter((leaders) => leaders.id === id)[0])
-      }, 2000);
-    });
+  getLeader(id: string): Observable<Leader>{
+    return of(LEADERS.filter((leaders) => leaders.id === id)[0]).pipe(delay(2000));
+    // return new Promise(resolve=> {
+    //   setTimeout(() => {
+    //     resolve(LEADERS.filter((leaders) => leaders.id === id)[0])
+    //   }, 2000);
+    // });
   }
 
-  getFeaturedLeader() : Promise<Leader>{
-    return new Promise(resolve=> {
-      setTimeout(() => {
-        resolve(LEADERS.filter((leaders) => leaders.featured)[0])
-      }, 2000);
-    });
+  getFeaturedLeader() : Observable<Leader>{
+    return of(LEADERS.filter((leaders) => leaders.featured)[0]);
+    // return new Promise(resolve=> {
+    //   setTimeout(() => {
+    //     resolve(LEADERS.filter((leaders) => leaders.featured)[0])
+    //   }, 2000);
+    // });
   }
 }
