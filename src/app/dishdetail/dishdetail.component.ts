7307e23dd1c0e0070ee56c109731896940c95d06
Kiruba Shankar Jayakumar <jkiruba.sankar@gmail.com>
2022-01-17 10:57:31 +0530

    angular assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
- 
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -14,12 +16,66 @@ import { switchMap } from 'rxjs/operators';
 
 export class DishdetailComponent implements OnInit {
 
+    feedbackForm: FormGroup;
     dish: Dish;
     dishIds: string[];
     prev: string;
     next: string;
+    comment: Comment;
+    @ViewChild('fform') feedbackformDirective;
 
-  constructor(private dishService: DishService, private location: Location, private route: ActivatedRoute) { }
+    formErrors = {
+      'author': '',
+      'comment': ''
+    }
+
+    validationMessages = {
+      'author' :{
+        'required': 'Author Name is required',
+        'minlength' : 'Author name must be at least 2 characters long',
+      },
+      'comment' :{
+        'required': 'comment is required'
+      }
+    }
+
+  constructor(private dishService: DishService, private location: Location, private route: ActivatedRoute, private fb: FormBuilder) { 
+    this.createFeedbackForm();
+  }
+
+  createFeedbackForm(){
+    this.feedbackForm = this.fb.group({
+      author: [ '', [Validators.required, Validators.minLength(2)]],
+      comment: [ '', [Validators.required]],
+      rating: '1'
+    });
+
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();//(re)set form validation messages
+  }
+  
+  onValueChanged(data? : any){
+    if(!this.feedbackForm) {
+      return;
+    }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        //clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid){
+          const messages = this.validationMessages[field];
+          for(const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field] += messages[key] + '';
+            }
+          }
+        }
+      }
+    }
+  }
 
   ngOnInit() {
     debugger;
@@ -43,5 +99,24 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void{
     this.location.back();
+  }
+
+  onSubmit(){   
+
+    this.comment = this.feedbackForm.value;
+
+    const d = new Date();
+    let commentDate = d.toISOString();
+    this.comment.date = commentDate;
+
+    this.dish.comments.push(this.feedbackForm.value);
+    
+    this.feedbackForm.reset({
+      author: '',
+      comment: '',
+      rating: '1'
+    });
+
+    this.feedbackformDirective.resetForm();
   }
 }
